1489b8d89ade486138e17ac25ec070d3edfc444a
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-10-21 19:39:56 -0300

    adicionando exercicio 2 de media ponderada

---
+++ media-2/script.js
@@ -0,0 +1,16 @@
+const rs = require('readline-sync')
+
+const A = Number(rs.question("Digite a primeira nota: "))
+const B = Number(rs.question("Digite a segunda nota: "))
+const C = Number(rs.question("Digite a terceira nota: "))
+const verificaNota = A >= 0 && A <= 10 && B >= 0 && B <= 10 && C >= 0 && C <= 10
+
+if(verificaNota){
+  const pA = 2
+  const pB = 3
+  const pC = 5
+  const media = (((A * pA) + (B * pB) + (C * pC)) / (pA + pB + pC)).toFixed(1)
+  console.log(`Média = ${media}`)
+}else{
+  console.log("Insira notas de 0 a 10")
+}
