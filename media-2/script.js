9daf43d20c7d6413409e76a17968790fb6a069de
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-10-21 21:04:01 -0300

    alterando estrutura do projeto

--- media-2/script.js
+++ media-2/script.js
@@ -3,12 +3,11 @@ const rs = require('readline-sync')
 const A = Number(rs.question("Digite a primeira nota: "))
 const B = Number(rs.question("Digite a segunda nota: "))
 const C = Number(rs.question("Digite a terceira nota: "))
+const pA = 2
+const pB = 3
+const pC = 5
 const verificaNota = A >= 0 && A <= 10 && B >= 0 && B <= 10 && C >= 0 && C <= 10
-
 if(verificaNota){
-  const pA = 2
-  const pB = 3
-  const pC = 5
   const media = (((A * pA) + (B * pB) + (C * pC)) / (pA + pB + pC)).toFixed(1)
   console.log(`Média = ${media}`)
 }else{
